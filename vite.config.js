4d6ebc989d807b5b19d205b6c832f541cea2bab5
Aishwarya Kumari <improaish@gmail.com>
2026-03-22 18:19:03 +0530

    Configure base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/resumebuilder/' : '/airesume/',
+  base: command === 'build' ? '/AutoResume/' : '/',
 }))
